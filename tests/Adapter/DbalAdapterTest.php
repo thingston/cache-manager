ec37324a0735c074852abb38d2625c10922cb3da
Pedro Ferreira <pedro@thingston.com>
2022-07-04 19:39:51 +0100

    Improve tests

--- tests/Adapter/DbalAdapterTest.php
+++ tests/Adapter/DbalAdapterTest.php
@@ -7,6 +7,7 @@ namespace Thingston\Tests\Cache\Adapter;
 use PHPUnit\Framework\TestCase;
 use Thingston\Cache\Adapter\CacheAdapterInterface;
 use Thingston\Cache\Adapter\DbalAdapter;
+use Thingston\Cache\Exception\InvalidArgumentException;
 
 final class DbalAdapterTest extends TestCase
 {
@@ -15,7 +16,20 @@ final class DbalAdapterTest extends TestCase
     protected function createAdapter(): CacheAdapterInterface
     {
         return new DbalAdapter([
-            'url' => 'sqlite:///:memory:',
+            'url' => 'sqlite:///' . sys_get_temp_dir() . '/' . uniqid() . '.cache',
         ]);
+    }
+
+    public function testBadAdapter(): void
+    {
+        $adapter = new DbalAdapter([
+            'url' => 'foo',
+        ]);
+
+        $this->assertFalse($adapter->set('foo', 'bar', 60));
+        $this->assertFalse($adapter->delete('foo'));
+
+        $this->expectException(InvalidArgumentException::class);
+        $this->assertFalse($adapter->has('foo'));
     }
 }
